6ed0478bb34039ba5f3e60073864729b071b0112
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2024-01-23 07:25:22 -0800

    sorting algorithms

--- sort.h
+++ sort.h
@@ -15,6 +15,13 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+
+typedef struct deck_node {
+    int value;
+    struct deck_node *next;
+    struct deck_node *prev;
+} deck_node_t;
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
@@ -28,5 +35,8 @@ void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 void sort_deck(deck_node_t **deck);
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+
 
 #endif /* SORT_H */
